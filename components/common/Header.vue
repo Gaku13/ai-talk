555c3c96b8ab386fe70bc72d70d941e284f01d5a
gaku13 <gaku@bulbcorp.jp>
2023-04-16 15:28:20 +0900

    first commit

---
+++ components/common/Header.vue
@@ -0,0 +1,146 @@
+<template lang="pug">
+  // headroom
+  header
+    nuxt-link.logo( to="/" )
+      Logo
+    nav
+      // a.nav-button( href="#" @click="click" )
+        | menu
+      .nav-container
+        // a.nav-close( href="#" @click="close" )
+          X
+        ul
+          li
+            nuxt-link( v-scroll-to="feature" to ) できること
+          li
+            nuxt-link( v-scroll-to="faq" to ) よくある質問
+      a.download-button( href="#" target="_blank" rel="noopener noreferrer nofollow" ) ダウンロード
+</template>
+
+<script>
+import Logo from '~/static/images/logo.svg?inline'
+import X from '~/static/images/icon/x.svg?inline'
+
+import { headroom } from 'vue-headroom'
+
+export default {
+  components: {
+    Logo,
+    X,
+    headroom,
+  },
+  data() {
+    return {
+      feature: {
+        el: '#feature',
+        offset: -80,
+        onStart: function(el) {
+          var nav;
+          nav = document.querySelector('.nav-container');
+          return nav.classList.remove('show');
+        }
+      },
+      faq: {
+        el: '#faq',
+        offset: -80,
+        onStart: function(el) {
+          var nav;
+          nav = document.querySelector('.nav-container');
+          return nav.classList.remove('show');
+        }
+      }
+    };
+  },
+  methods: {
+    click: function() {
+      var nav;
+      nav = document.querySelector('.nav-container');
+      return nav.classList.add('show');
+    },
+    close: function() {
+      var nav;
+      nav = document.querySelector('.nav-container');
+      return nav.classList.remove('show');
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  header
+    position fixed
+    top 0
+    left 0
+    display flex
+    align-items center
+    width: 100%
+    height 64px
+    padding 0 24px
+    background $white
+    transition transform .75s .25s ease, opacity .75s .25s ease, visibility .75s .25s
+    box-shadow: 0 1px 10px rgba(0, 0, 0, .1)
+    z-index $zindex-header
+    @media (max-width: $phone)
+      justify-content: space-between
+      height 50px
+      padding 0 16px
+  .logo
+    svg
+      width 151px
+      height auto
+      vertical-align bottom
+  nav
+    display: flex
+    align-items: center
+    justify-content: space-between
+    width: 100%
+    @media (max-width: $phone)
+      width: auto
+    .nav-button
+      display none
+      line-height 18px
+      visibility hidden
+      font-weight 700
+      letter-spacing .1rem
+      @media (max-width: $phone)
+        display block
+        visibility visible
+    .nav-container
+      position relative
+      margin-left: 24px
+      visibility visible
+    ul
+      display flex
+      align-items center
+      justify-content center
+      list-style none
+      margin 0
+      padding 0
+      li
+        padding 0 20px
+        a
+          color $text
+          font-size 14px
+          letter-spacing .025rem
+          text-decoration none
+          transition color .3s
+          &:hover
+            color $primary
+      @media (max-width: $phone)
+        display none
+    a.download-button
+      display: block
+      line-height: 20px
+      padding: 8px 12px
+      color: $white
+      font-size: 14px
+      font-weight: bold
+      text-align: center
+      background-color: $primary
+      transition: background-color .2s
+      border-radius: 4px
+      min-width: 130px
+      &:hover
+        background-color: $space
+</style>
+
---
+++ nuxt.config.js
@@ -0,0 +1,120 @@
+const environment = process.env.NODE_ENV || 'local'
+const env = require(`./env/${environment}.js`)
+
+module.exports = {
+  generate: {
+    fallback: true,
+    dir: 'docs'
+  },
+  mode: 'spa',
+  target: 'static',
+  router: {
+    base: env.BASE_URL
+  },
+  head: {
+    title: 'おしゃべりAI',
+    htmlAttrs: {
+      lang: 'ja'
+    },
+    meta: [
+      {
+        charset: 'utf-8'
+      }, {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      }, {
+      }, {
+        name: 'robots',
+        content: 'noindex'
+      }, {
+      }, {
+        name: 'googlebot',
+        content: 'noindex'
+      }, {
+        hid: 'description',
+        name: 'description',
+        content: 'おしゃべりAI'
+      }, {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website'
+      }, {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'Double O Capital Ltd.'
+      }, {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://double-o.jp'
+      }, {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'おしゃべりAI'
+      }, {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'なんでも話せる、なんでも聞ける、AIの友達。おしゃべりAI'
+      }, {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://gaku13.github.io/doubleocapital/og-image.png'
+      }, {
+        name: 'application-name',
+        content: 'おしゃべりAI'
+      },
+      { name: "msapplication-TileColor", content: "#ffffff" },
+      { name: "msapplication-TileImage", content: `${env.BASE_URL}mstile-150x150.png` },
+      { name: "msapplication-config", content: `${env.BASE_URL}browserconfig.xml` },
+      { name: "theme-color", content: "#ffffff" }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: `${env.BASE_URL}favicon.ico` },
+      { rel: 'apple-touch-icon', type: 'image/png', href: `${env.BASE_URL}apple-touch-icon.png` },
+      { rel: "icon", type: "image/png", href: `${env.BASE_URL}favicon-16x16.png`, sizes: "16x16" },
+      { rel: "icon", type: "image/png", href: `${env.BASE_URL}favicon-32x32.png`, sizes: "32x32" },
+      { rel: "icon", type: "image/png", href: `${env.BASE_URL}android-chrome-192x192.png`, sizes: "192x192" },
+      { rel: "icon", type: "image/png", href: `${env.BASE_URL}android-chrome-512x512.png`, sizes: "512x512" },
+      { rel: "manifest", href: `${env.BASE_URL}site.webmanifest` },
+      { rel: "mask-icon", href: `${env.BASE_URL}safari-pinned-tab.svg`, color: "#5bbad5" },
+      { rel: "shortcut icon", href: `${env.BASE_URL}favicon.ico` }
+    ]
+  },
+  //loading: false,
+  loading: {
+    color: '#6768F5',
+    height: '2px',
+    throttle: 0
+  },
+  css: [
+    '~/assets/css/style.styl'
+  ],
+  plugins: [
+    {
+      src: '~/plugins/vue-parallax-js',
+      ssr: false
+    }, '~/plugins/scroll', '~/plugins/vue-scrollto'
+  ],
+  modules: [
+    '@nuxtjs/svg',
+    '@nuxtjs/style-resources',
+    [
+      '@nuxtjs/google-analytics', {
+        id: ''
+      }
+    ],
+  ],
+  styleResources: {
+    stylus: './assets/css/_variables.styl'
+  },
+  /*
+  build: extend = function(config, ctx) {
+    plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    return pass;
+  }
+  */
+  build: {
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    }
+  }
+}
